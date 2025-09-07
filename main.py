ff9855bca1f1f2d9a0ab41304f32bf88d460bc3d
Jose Jaime Felix Garcia <josejaimefelixgarcia@gmail.com>
2025-09-07 18:30:09 +0800

    Scaffold conflict with venv fixed

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+
+from agents import Agent
+from agents import Runner
+
+
+
+cashier_agent=Agent(
+    name = "cashier agent",
+    instructions = "you are a cashier"
+)
+
+
+
 def main():
     print("Hello from agents-unit-testing!")
-
 
 if __name__ == "__main__":
     main()
